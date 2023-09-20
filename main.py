102a753e515fb5990a61338210fd390f9a72602f
kazuhiko1979 <challengevolley1979@gmail.com>
2023-09-20 18:31:43 +0900

    20230920

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from flask.views import MethodView
-from wtforms import Form
+from wtforms import Form, StringField, SubmitField
+from flask import Flask, render_template
 
+app = Flask(__name__)
 
 class HomePage(MethodView):
-	pass
+
+	def get(self):
+		return render_template('index.html')
 
 
 class BillFormPage(MethodView):
-	pass
+
+	def get(self):
+		bill_form = BillForm()
+		return render_template('bill_form_page.html',
+							   billform=bill_form)
 
 
 class ResultPage(MethodView):
@@ -15,4 +23,21 @@ class ResultPage(MethodView):
 
 
 class BillForm(Form):
-	pass
+	amount = StringField("Bill Amount: ")
+	period = StringField("Bill Period: ")
+
+	name1 = StringField("Name: ")
+	days_in_house1 = StringField("Days in the house: ")
+
+	name2 = StringField("Name: ")
+	days_in_house2 = StringField("Days in the house: ")
+
+	button = SubmitField("Calculate")
+
+
+app.add_url_rule('/',
+				 view_func=HomePage.as_view('home_page'))
+app.add_url_rule('/bill_form',
+				 view_func=BillFormPage.as_view('bill_form_page'))
+
+app.run(debug=True)
