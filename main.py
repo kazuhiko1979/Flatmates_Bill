a027336b1889ee3e95734af0731c30651c738986
kazuhiko1979 <challengevolley1979@gmail.com>
2023-09-26 15:19:25 +0900

    update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask.views import MethodView
 from wtforms import Form, StringField, SubmitField
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flatmates_bill import flat
 
 app = Flask(__name__)
 
@@ -18,19 +19,32 @@ class BillFormPage(MethodView):
 							   billform=bill_form)
 
 
-class ResultPage(MethodView):
-	pass
+class ResultsPage(MethodView):
 
+	def post(self):
+		billform = BillForm(request.form)
+		amount = billform.amount.data
+		period = billform.period.data
+
+		the_bill = flat.Bill(float(billform.amount.data), billform.period.data)
+		flatmate1 = flat.Flatmate(billform.name1.data, float(billform.days_in_house1.data))
+		flatmate2 = flat.Flatmate(billform.name2.data, float(billform.days_in_house2.data))
+
+		return render_template('results.html',
+							   name1=flatmate1.name,
+							   amount1=flatmate1.pays(the_bill, flatmate2),
+							   name2=flatmate2.name,
+							   amount2=flatmate2.pays(the_bill, flatmate1))
 
 class BillForm(Form):
-	amount = StringField("Bill Amount: ")
-	period = StringField("Bill Period: ")
+	amount = StringField("Bill Amount: ", default="100")
+	period = StringField("Bill Period: ", default="December 2023")
 
-	name1 = StringField("Name: ")
-	days_in_house1 = StringField("Days in the house: ")
+	name1 = StringField("Name: ", default="John")
+	days_in_house1 = StringField("Days in the house: ", default=20)
 
-	name2 = StringField("Name: ")
-	days_in_house2 = StringField("Days in the house: ")
+	name2 = StringField("Name: ", default="Marry")
+	days_in_house2 = StringField("Days in the house: ", default=12)
 
 	button = SubmitField("Calculate")
 
@@ -39,5 +53,8 @@ app.add_url_rule('/',
 				 view_func=HomePage.as_view('home_page'))
 app.add_url_rule('/bill_form',
 				 view_func=BillFormPage.as_view('bill_form_page'))
+
+app.add_url_rule('/results',
+				 view_func=ResultsPage.as_view('result_page'))
 
 app.run(debug=True)
